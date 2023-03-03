b27446f7fb71de1bcb163efd038769126af5f671
boy1434@naver.com <boy1434@naver.com>
2023-03-03 09:34:53 +0900

    계산기

---
+++ Momentum/app.js
@@ -0,0 +1,19 @@
+const calculator = {
+    add: function(a,b) {
+        console.log(a + b);
+    },
+    minus: function(a,b) {
+        console.log(a - b);
+    },
+    multi: function(a,b) {
+        console.log(a * b);
+    },
+    divide: function(a,b) {
+        console.log(a / b);
+    }
+}
+
+calculator.add(1,5)
+calculator.minus(5,1)
+calculator.multi(2,4)
+calculator.divide(10,2)
